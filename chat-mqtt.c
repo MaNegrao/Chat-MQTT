b3b2f747dd7a1435c36991585a1a35353461317d
MaNegrao <matheus.dnegrao@gmail.com>
2021-05-20 09:37:37 -0300

    refazendo

--- chat-mqtt.c
+++ chat-mqtt.c
@@ -3,27 +3,26 @@
 void menu(){
     printf("-- Escolha uma oção --\n");
     printf("1. Iniciar um chat com um usuario\n");
-    printf("2. Entrar em um grupo \n");
-    printf("3. Enviar mensagem em um grupo \n");
+    printf("2. Enviar mensagem\n");
+    printf("3. Entrar em um grupo \n");
+    printf("4. Enviar mensagem em um grupo \n");
 	printf("4. Logout \n");
 }
 
-void logout(){
-    char user[4];
+int pub_msg(MQTTClient client, char * topic, char * payload){
+    int rc;
 
-    online_users_file = fopen("online-users.txt", "r+");
+    MQTTClient_message message = MQTTClient_message_initializer;
 
-    flockfile(online_users_file);
+    message.payload = payload;
+    message.payloadlen = sizeof(payload);
+    message.qos = QOS;
 
-    while(fgets(user, 4, online_users_file)){
-        if(!strcmp(user, USER_ID)){
-            fseek(online_users_file, -3, SEEK_CUR);
-            fprintf(online_users_file, "XX");
-        }
+    if((rc = MQTTClient_publishMessage(client, topic, &message, NULL)) != MQTTCLIENT_SUCCESS){
+        printf("Failed to publish message, return code %d\n", rc);
+        MQTTClient_destroy(&client);
+        exit(0);
     }
-
-    funlockfile(online_users_file);
-    fclose(online_users_file);
 
 }
 
@@ -48,44 +47,36 @@ int ver_usr(char * usr_cmp){
         return 0;
 }
 
-void *req_conn(){
-        char sel[5];
-        
-        printf("Solicitação de conversa recebida! Deseja aceitar? (sim\\nao)");
-        fflush(stdout);
-        pthread_mutex_lock(&scan_mutex);            
-            fgets(sel, sizeof(sel), stdin);  
-        pthread_mutex_unlock(&scan_mutex);
+void req_conn(char * rec_user, char * topic_rec, char * topic_chat){
+    sprintf(topic_chat, "%d_%.2s_%.2s_Chat", session_id, rec_user, USER_ID);
+    sprintf(topic_rec, "%.2s_Client", rec_user);
+    MQTTClient_subscribe(client, topic_chat, QOS);
 
-        if(!strcmp(sel, "sim\n") || !strcmp(sel, "SIM\n")){
-            return (void *) 1;
-        }
-        else{
-            return (void *) 0;
-        }   
+    users_topic = fopen(users_topic, "a+");
 
+    fgets(topic_chat, sizeof(topic_chat), users_topic);
+
+    fclose(users_topic);
 }
 
-void *new_conn(){
-    
+void *handle_msg_arrvd(char * rec_user, char * topic, MQTTClient client){
+
+    if(!strcmp(topic, USER_TOPIC_CONTROL)){
+        printf("%.2s Inicou um chat com você\n", rec_user);
+
+            req_conn(rec_user, topic_rec, topic_chat);
+            pub_msg(client, topic_rec, topic_chat);
+        }
+    }
+    else{
+        printf("%s\n", topic);
+    }
+
 }
 
 int msg_arrvd(void *context, char *topic_name, int topic_len, MQTTClient_message *message){
-    void ** select;
-
-    if(!strcmp(topic_name, USER_TOPIC_CONTROL)){
-        if(!strcmp(message->payload, "conn_req")){
-
-            pthread_create(&req_conn_thread,NULL,req_conn,NULL);
-
-            pthread_join(req_conn_thread, select);
-
-            if(select){
-                new_conn(&topic_name);
-            } 
-        }   
-
-    }
+    
+    handle_msg_arrvd(message->payload, topic_name, client_mqtt);
 
     // printf("     topic: %s\n", topic_name);
     // printf("   message: %.*s\n", message->payloadlen, (char*)message->payload);
@@ -98,22 +89,6 @@ void conn_lost(void *context, char *cause){
     printf("\nConexão perdida. Erro: %s\n", cause);
 }
 
-int pub_msg(MQTTClient client, char * topic, char * payload){
-    int rc;
-
-    MQTTClient_message message = MQTTClient_message_initializer;
-
-    message.payload = payload;
-    message.payloadlen = sizeof(payload);
-    message.qos = QOS;
-
-    if((rc = MQTTClient_publishMessage(client, topic, &message, NULL)) != MQTTCLIENT_SUCCESS){
-        printf("Failed to publish message, return code %d\n", rc);
-        MQTTClient_destroy(&client);
-        exit(0);
-    }
-
-}
 
 void ini_chat(MQTTClient m_client){
 
@@ -124,21 +99,35 @@ void ini_chat(MQTTClient m_client){
     __fpurge(stdin);
     fgets(rec_id, sizeof(rec_id), stdin);
 
-    if(ver_usr(rec_id)){
-        printf("O usuario não está online\n");
-        return;
-    }
-    else{
-        strncat(rec_topic, rec_id, 2);
-        strcat(rec_topic, "_Control");
-        //printf("%s", rec_topic);
-    }
-    
+    strncat(rec_topic, rec_id, 2);
+    strcat(rec_topic, "_Control");
+
     printf("Requisitando inicio de sessão...\n");
-
-    pub_msg(m_client, rec_topic, "conn_req");
-
+    pub_msg(m_client, rec_topic, USER_ID);
 }
+
+// void sub_group(MQTTClient g_client){
+//     int sel, rc;
+//     char group_in_file[2];
+
+//     printf("Em qual dos grupos abaixo você deseja entrar?\n");
+//     printf("1 - Grupo dos Jedi's Ocultos\n");
+//     printf("2 - Grupo dos Fãs de Formula 1\n");
+//     printf("3 - Grupo dos jogadores de Overwatch\n");
+//     printf("4 - Grupo dos Assinantes de iRacing\n");
+//     printf("5 - Grupo dos Jedi's Ocultos\n");
+    
+//     scanf("%d", &sel);
+
+//     if((rc = MQTTClient_subscribe(g_client, sel, QOS)) != MQTTCLIENT_SUCCESS)){
+//         printf("Falha ao assinar o tópico, Erro: %d\n", rc);
+//         MQTTClient_destroy(&g_client);
+//         exit(0);
+//     }
+//     else{
+//         printf("Cadastrado com sucesso!\n");
+//     }
+// }
 
 void *main(){
 
@@ -149,22 +138,6 @@ void *main(){
     
     __fpurge(stdin);
     fgets(USER_ID, sizeof(USER_ID), stdin);
-    
-    online_users_file = fopen("online-users.txt", "a+");
-
-    flockfile(online_users_file);
-
-    while(fgets(user_in_file, 4, online_users_file)){
-        if(!strcmp(user_in_file, USER_ID)){
-            printf("Você já está online!\n");
-            exit(0);
-        }
-    }
-
-    fputs(USER_ID, online_users_file);
-
-    funlockfile(online_users_file);
-    fclose(online_users_file);
 
     strncat(USER_TOPIC_CONTROL, USER_ID, 2);
     strcat(USER_TOPIC_CONTROL, "_Control");
@@ -174,15 +147,15 @@ void *main(){
 
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 
+    // conn_opts.keepAliveInterval = 20;
+    // conn_opts.cleansession = 1;
+
 
     if((rc = MQTTClient_create(&client_mqtt, ADDRESS, USER_TOPIC_CONTROL, 
     MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS){
         printf("Falha na criação do tópico do cliente. Erro: %d\n", rc);
         exit(0);
     }
-
-    //conn_opts.keepAliveInterval = 20;
-    //conn_opts.cleansession = 1;
     
     if((rc = MQTTClient_setCallbacks(client_mqtt, NULL, conn_lost, msg_arrvd, NULL)) != MQTTCLIENT_SUCCESS){
         printf("Erro ao setar os callbacks. Erro: %d\n", rc);
@@ -213,18 +186,19 @@ void *main(){
 
     do
     {
+        //system("clear");
         pthread_mutex_lock(&scan_mutex);
         menu();
-        scanf("%d", &sel);
+        scanf(" %d ", &sel);
         pthread_mutex_unlock(&scan_mutex);
-        
-
 
         switch (sel){
             case 1:
                 ini_chat(client_mqtt);
+                break;
             
             case 2:
+                //sub_group(client_mqtt);
                 break;
 
             case 3:
@@ -236,11 +210,9 @@ void *main(){
                 break;
 
             default:
+                printf("Opção inválida!\n");
                 break;
         }
-    } while (sel != 4);
-    
-
-    
+    } while (sel != 4);  
 }
 
--- chat-mqtt.h
+++ chat-mqtt.h
@@ -14,18 +14,21 @@
 #define GP1_TOPIC_ID "Secret_Jedis"
 #define GP2_TOPIC_ID "Formula_1_Fans"
 #define GP3_TOPIC_ID "Overwatch_Players"
-#define GP4_TOPIC_ID "iRacing_Players"
+#define GP4_TOPIC_ID "iRacing_Subscribers"
 #define GP5_TOPIC_ID "Coding_Lovers"
 
 char USER_ID[4];
-char USER_TOPIC_CONTROL[12];
-char USER_TOPIC_CLIENT[12];
+char USER_TOPIC_CONTROL[12]="";
+char USER_TOPIC_CLIENT[12]="";
 
-int session_id;
 
-FILE *online_users_file;
+int session_id = 1;
+
+FILE *users_topic;
+FILE *subs_groups_file;
 
 pthread_mutex_t scan_mutex;
 pthread_t req_conn_thread;
+pthread_t handle_conn_thread;
 
 MQTTClient client_mqtt;
